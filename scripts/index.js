e0d78939e1b68bd9843684dcc8a110c1d82daa71
Maxim Musienko <musmaxim.musienko@yandex.ru>
2021-03-21 21:32:01 +0300

    style: дописать текст в alt

--- scripts/index.js
+++ scripts/index.js
@@ -5,8 +5,8 @@ let closePopupOverlay = document.querySelector('.popup__overlay');
 let ProfileName = document.querySelector('.profile__name');
 let ProfileWork = document.querySelector('.profile__work');
 let formElement = document.querySelector('.save');
-let nameInput = document.querySelector('.form__text_el_name');
-let jobInput = document.querySelector('.form__text_el_work');
+let nameInput = document.querySelector('.form_text-name');
+let jobInput = document.querySelector('.form_text-work');
 
 function openPopup() {
     popup.classList.add('popup_visible');
