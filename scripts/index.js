40037f43dcf4c255f7a729dfb0fce91f1a2a4b0c
Maxim Musienko <musmaxim.musienko@yandex.ru>
2021-03-21 20:54:04 +0300

    отправить на проверку

---
+++ scripts/index.js
@@ -0,0 +1,43 @@
+let popup = document.querySelector('.popup');
+let openPopupBtn = document.getElementById('open_popup_btn');
+let closePopupBtn = document.querySelector('.popup__close-button');
+let closePopupOverlay = document.querySelector('.popup__overlay');
+let ProfileName = document.querySelector('.profile__name');
+let ProfileWork = document.querySelector('.profile__work');
+let formElement = document.querySelector('.save');
+let nameInput = document.querySelector('.form__text_el_name');
+let jobInput = document.querySelector('.form__text_el_work');
+
+function openPopup() {
+    popup.classList.add('popup_visible');
+    nameInput.value = ProfileName.textContent;
+    jobInput.value = ProfileWork.textContent;
+}
+
+function closePopup() {
+    popup.classList.remove('popup_visible');
+}
+
+function formSubmitHandler(evt) {
+    evt.preventDefault();
+    ProfileName.textContent = nameInput.value;
+    ProfileWork.textContent = jobInput.value;
+    closePopup();
+}
+
+formElement.addEventListener('click', formSubmitHandler);
+
+openPopupBtn.addEventListener('click', function() {
+    openPopup();
+
+});
+
+closePopupBtn.addEventListener('click', function() {
+    closePopup();
+
+});
+
+closePopupOverlay.addEventListener('click', function() {
+    closePopup();
+
+});
